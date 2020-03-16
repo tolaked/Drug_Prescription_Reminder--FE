f80222f9df08c421b4f915264e27f406d64ef4ac
Akere Adetola <adetolaakere1@gmail.com>
2020-03-16 09:48:06 +0100

    header

--- prescription-reminder/src/App.js
+++ prescription-reminder/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
-
+import Header from './reusables/Header'
 function App() {
   return (
     <div className="App">
-     
+     <Route exact path='/' component={Header}/>
     </div>
   );
 }
