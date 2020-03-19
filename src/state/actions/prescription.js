2e682b243a7672c4d5036f1136cc45ba852a6c9a
Akere Adetola <adetolaakere1@gmail.com>
2020-03-19 21:20:40 +0100

    corrected heroku url link

--- src/state/actions/prescription.js
+++ src/state/actions/prescription.js
@@ -29,7 +29,7 @@ export const deletePrescriptionSuccess =(payload)=>({
 
 export const addPrescription = (data) => dispatch => {
   const token = Cookie.get('token');
-  axios({ method: 'POST', url: 'http://cors-anywhere.herokuapp.com/https://dashboard.heroku.com/apps/drug-prescription-app/api/v1/prescription/add',data, headers:{'Access-Control-Allow-Origin': '*',
+  axios({ method: 'POST', url: 'https://drug-prescription-app.herokuapp.com/api/v1/prescription/add',data, headers:{'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json',
     'Authorization': token,}})
    
@@ -44,14 +44,13 @@ export const getPrescriptions = () => dispatch => {
   dispatch(getPrescriptionRequest(true));
   const token = Cookie.get('token');
   axios(
-    { method: 'GET', url: 'http://cors-anywhere.herokuapp.com/https://dashboard.heroku.com/apps/drug-prescription-app/api/v1/prescription', headers:{'Access-Control-Allow-Origin': '*',
+    { method: 'GET', url: 'https://drug-prescription-app.herokuapp.com/api/v1/prescription', headers:{'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json',
     'Authorization': token,}})
    
     .then(({ data }) => {
       
       dispatch(getPrescriptionSuccess(data.prescription));
-      console.log('OMOOO',data)
     })
     .catch(error => {
       console.log(error.response)
@@ -63,7 +62,7 @@ export const fetchSinglePrescription = (id) => dispatch => {
   dispatch(getSinglePresriptionSuccess(true));
   const token = Cookie.get('token');
   axios(
-    { method: 'GET', url: `http://cors-anywhere.herokuapp.com/https://dashboard.heroku.com/apps/drug-prescription-app/api/v1/prescription/find/${id}`, headers:{'Access-Control-Allow-Origin': '*',
+    { method: 'GET', url: `https://drug-prescription-app.herokuapp.com/api/v1/prescription/find/${id}`, headers:{'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json',
     'Authorization': token}})
    
@@ -79,7 +78,7 @@ export const fetchSinglePrescription = (id) => dispatch => {
 export const deletePrescription = (id) => dispatch => {
   const token = Cookie.get('token');
   axios(
-    { method: 'DELETE', url: `https://dashboard.heroku.com/apps/drug-prescription-app/api/v1/prescription/${id}`, headers:{'Access-Control-Allow-Origin': '*',
+    { method: 'DELETE', url: `https://drug-prescription-app.herokuapp.com/api/v1/prescription/${id}`, headers:{'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json',
     'Authorization': token}})
    
